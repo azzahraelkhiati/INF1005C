bc19a312638e50b7c61dea224ab103182f7944ea
johnatangao <jonathangao12@gmail.com>
2019-09-19 09:36:45 -0400

    update exercice en classe

---
+++ ExerciceClasse/Exo3/Exo3.cpp
@@ -0,0 +1,16 @@
+/*
+Un programme qui lit les 7 lignes du fichier poeme.txt et affiche la troisieme ligne
+
+Soit le fichier poeme.txt
+La Cigale, ayant chante
+Tout l'ete
+Se trouva fort depourvue
+Quand la bise fut venue:
+Pas un seul petit morceau
+De mouche ou de vermisseau.
+Elle alla crier famine
+
+*/
+
+#include <iostream>
+#include <string>
